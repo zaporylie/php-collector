8e9c5d60b8246c6b6620403daa16cd9dda48a20f
Jakub Piasecki <jakub@nymedia.no>
2016-05-19 08:18:59 +0200

    Adjust Invoice

---
+++ src/Collector/Request/AdjustInvoice.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Collector\Request;
+
+use Collector\Data\HeaderTrait;
+use Collector\Data\InvoiceTrait;
+use Collector\InvoiceService;
+use Collector\ServiceInterface;
+
+/**
+ * Class AdjustInvoice.
+ */
+class AdjustInvoice extends InvoiceService implements ServiceInterface
+{
+    use HeaderTrait;
+    use InvoiceTrait;
+
+    const METHOD = 'AdjustInvoice';
+
+    /**
+     * @var string
+     */
+    protected $ArticleId;
+
+    /**
+     * @var string
+     */
+    protected $Description;
+
+    /**
+     * @var float
+     */
+    protected $Amount;
+
+    /**
+     * @var float
+     */
+    protected $Vat;
+
+    /**
+     * @param string $ArticleId
+     */
+    public function setArticleId($ArticleId)
+    {
+        $this->ArticleId = $ArticleId;
+    }
+
+    /**
+     * @param string $Description
+     */
+    public function setDescription($Description)
+    {
+        $this->Description = $Description;
+    }
+
+    /**
+     * @param float $Amount
+     */
+    public function setAmount($Amount)
+    {
+        $this->Amount = $Amount;
+    }
+
+    /**
+     * @param float $Vat
+     */
+    public function setVat($Vat)
+    {
+        $this->Vat = $Vat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return self::METHOD;
+    }
+}
