4e02f8a471c35b81a5d41d909b26e5eb47bf3897
zaporylie <jakub@nymedia.no>
2016-06-05 15:01:23 +0200

    DateTime serialization test

---
+++ test/unit/Data/DateTimeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Collector\Data;
+
+class DateTimeTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $dateTime;
+
+    public function setUp()
+    {
+        $this->dateTime = new DateTime();
+        $this->dateTime->setTimezone(new \DateTimeZone('Europe/London'));
+        $this->dateTime->setTimestamp(946684800);
+    }
+
+    public function testSerialization() {
+        $serialized = json_encode($this->dateTime);
+        $this->assertEquals(json_encode("2000-01-01T00:00:00+00:00"), $serialized);
+    }
+}
