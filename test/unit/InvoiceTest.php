6c5225192006620c017169f68f3e4934282ae43e
Jakub Piasecki <jakub@nymedia.no>
2016-06-09 16:14:33 +0200

    Start testing services

---
+++ test/unit/InvoiceTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Collector;
+
+use Collector\Data\Country;
+
+class InvoiceTest extends Data\InvoiceTest
+{
+    /**
+     * @var int
+     */
+    protected $invoiceId;
+
+    /**
+     * @var ClientInterface
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $countryCode;
+
+    /**
+     * @var Invoice
+     */
+    protected $invoiceService;
+
+    /**
+     * @var object
+     */
+    protected $response;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->response = new \stdClass();
+        $this->response->InvoiceNo = 1234;
+        $this->invoiceId = 4321;
+        $this->countryCode = Country::NORWAY;
+        $this->client = $this->getMockBuilder('\Collector\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->client->expects($this->any())
+            ->method('call')
+            ->willReturnCallback(function ($method) {
+                $this->response->method = $method;
+                return $this->response;
+            });
+        $this->invoiceService = new Invoice($this->client, $this->countryCode);
+    }
+
+    public function testAddInvoice()
+    {
+        $this->invoiceService->addInvoice($this->invoice);
+        $this->assertEquals('AddInvoice', $this->invoiceService->getLastResponse()->method);
+        $this->assertSame($this->response, $this->invoiceService->getLastResponse());
+    }
+
+    public function testActivateInvoice()
+    {
+        $this->invoiceService->activateInvoice();
+        $this->assertEquals('ActivateInvoice', $this->invoiceService->getLastResponse()->method);
+        $this->assertSame($this->response, $this->invoiceService->getLastResponse());
+    }
+}
